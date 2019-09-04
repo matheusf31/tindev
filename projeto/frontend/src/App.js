1e069b2e4a8039bb997ee3a689355c8bbc3ecda9
Matheus Filipe <mfcr031@gmail.com>
2019-09-04 15:33:44 -0300

    inicio do frontend  da aplicação

--- projeto/frontend/src/App.js
+++ projeto/frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+   <Login />
   );
 }
 
---
+++ projeto/frontend/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.css';
+
+import logo from '../assets/logo.png'
+
+export default function Login() {
+  return (
+    <img src={logo} alt='Tindev'></img>
+  )
+}
